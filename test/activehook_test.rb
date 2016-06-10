a695b2d91b53ddb6ea6a5efcd57c028f64705471
Nicholas Sweeting <nsweeting@gmail.com>
2016-06-10 06:40:19 -0400

    Fix naming.

--- test/activehook_test.rb
+++ test/activehook_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class ActivehookTest < Minitest::Test
+class ActiveHookTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Activehook::VERSION
+    refute_nil ::ActiveHook::VERSION
   end
 
   def test_it_does_something_useful
